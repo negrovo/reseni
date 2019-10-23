61256edd85f1c9abbd6b7dd502a990e4e7e195ab
negrovo <37615034+negrovo@users.noreply.github.com>
2019-10-23 10:51:36 +0200

    Update psql.sh

--- psql.sh
+++ psql.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 apt update
 apt upgrade -y
-apt-get install htop -y
+apt install sudo -y
+apt install postgresql -y
+su - postgresql
+psql -c 'CREATE DATABASE test;'
+psql -d test -c 'CREATE TABLE counter (count, int);'
+psql -d test -c 'UPDATE counter SET count=0;'
