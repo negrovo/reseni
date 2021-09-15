eee7ce712ac45f51d459cf7cfb85379992435941
negrovo <37615034+negrovo@users.noreply.github.com>
2021-09-15 15:57:13 +0200

    Delete psql.sh

--- psql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-apt update
-apt upgrade -y
-apt install sudo -y
-apt install postgresql -y
-su - postgresql
-psql -c 'CREATE DATABASE test;'
-psql -d test -c 'CREATE TABLE counter (count, int);'
-psql -d test -c 'UPDATE counter SET count=0;'
